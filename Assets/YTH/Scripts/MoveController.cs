a2b7ec7c90bcfe2a1f43c08cddc7e2dc6531c784
TaeHeon Ryoo <taroni55@naver.com>
2024-08-28 20:03:06 +0900

    feat 맵 생성, 플레이어 이동 구현

--- Assets/YTH/Scripts/MoveController.cs
+++ Assets/YTH/Scripts/MoveController.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] Rigidbody rigid;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float rate;
+
+    private bool y;
+    private void Update()
     {
-        
+        Move();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Move()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        bool y = Input.GetKeyDown(KeyCode.Space);
+
+
+        //Debug.Log($"{x}, {z}");
+
+        Vector3 moveDir = new Vector3(x, 0, z);
+        if (moveDir.magnitude > 1) //GetAxisRaw 가 아니어도 정규화 가능
+        {
+            moveDir.Normalize();
+        }
+
+        rigid.velocity = moveDir * moveSpeed;
+
+        Quaternion lookRot = Quaternion.LookRotation(moveDir);
+        transform.rotation = Quaternion.Lerp(transform.rotation, lookRot, rate * Time.deltaTime);
+
+        //점프
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        }
+
+
     }
 }
