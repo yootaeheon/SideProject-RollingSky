3afacc187c15edbab361d1270612edc0231cfcaa
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-09-12 21:21:56 +0900

    Feat: 속도 상승 아이템 구현

--- Assets/Develop/Scripts/LYJ_PlayerMove.cs
+++ Assets/Develop/Scripts/LYJ_PlayerMove.cs
@@ -9,9 +9,16 @@ public class LYJ_PlayerMove : MonoBehaviour
 
     [SerializeField] float MoveSpeed;
 
+    [SerializeField] float SpeedBoost; //부스트 속도 증가량
+
+    [SerializeField] float SpeedBoostTime; //속도 증가 지속 시간
+
     [SerializeField] Vector3 startPosition;  //플레이어의 처음 위치
 
     [SerializeField] Transform playerTransform;
+
+    private bool isSpeedBoosted = false; //현재 부스트가 적용 중인가?
+    private float originalMoveSpeed; //부스트 적용 전 (원래 이동 속도)를 저장
 
     //-----------------------------------------------------
     //----------------- 유니티 메시지 함수 -----------------
@@ -21,6 +28,7 @@ public class LYJ_PlayerMove : MonoBehaviour
     {
         startPosition = transform.position;
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        originalMoveSpeed = MoveSpeed; //처음엔 원래 이동 속도 저장하여 사용
     }
 
 
@@ -41,8 +49,11 @@ public class LYJ_PlayerMove : MonoBehaviour
 
         }
 
-        // 가만히 있어도 항상 앞으로 굴러감
-        transform.position += Vector3.forward * MoveSpeed * Time.deltaTime;
+        //isSpeedBoosted가 true이면 MoveSpeed에 SpeedBoost 곱한 속도로 이동, false이면 기본 속도로 이동
+        float currentSpeed = isSpeedBoosted ? MoveSpeed * SpeedBoost : MoveSpeed;
+        
+        //앞으로 계속 공을 굴림
+        transform.position += Vector3.forward * currentSpeed * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
@@ -72,6 +83,30 @@ public class LYJ_PlayerMove : MonoBehaviour
     public void Die()
     {
         //BSM_TrapCube에서 링크됨
+    }
+
+    //속도 부스터 적용하는 함수
+    public void ApplySpeedBoost()
+    {
+        //속도 부스트가 적용되어 있지 않은 상태면
+        if (!isSpeedBoosted)
+        {
+            //코루틴 시작
+            StartCoroutine(SpeedBoostCoroutine());
+        }
+    }
+
+    //정해진 시간 동안 속도 부스트를 적용하는 코루틴
+    private IEnumerator SpeedBoostCoroutine()
+    {
+        //시작하면 부스트 상태를 true로 설정하고
+        isSpeedBoosted = true;
+
+        //지정한 SpeedBoostTime만큼 적용되었다가
+        yield return new WaitForSeconds(SpeedBoostTime);
+        
+        //부스트 상태를 다시 false로 설정
+        isSpeedBoosted = false;
     }
 
 }
