021bf6d80ec2ac04dddc04336b262849faf825a6
TaeHeon Ryoo <taroni55@naver.com>
2024-10-04 18:39:51 +0900

    [FEAT] GameManager 추가 기능 구현
    게임 시작 시, 보석 획득 시 카운팅 기능, 게임 시간 기록 기능 구현

--- Assets/Develop/Scripts/GameManager.cs
+++ Assets/Develop/Scripts/GameManager.cs
@@ -9,6 +9,15 @@ public class GameManager : MonoBehaviour
     public int stageGem; //한 스테이지 내에서 획득한 보석 갯수
     public int totalGem; //총 보석 갯수
 
+    public bool isGameOver = false;
+
+    private float startTime;
+    private float elapsedTime;
+
+   
+    public float bestTime = float.MaxValue;   // 최단 시간 기록 
+                                              // 첫 플레이어 기록 = 최단 시간
+
     private void Awake()
     {
         if (Instance == null)
@@ -22,4 +31,40 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public void StartGame() // 게임 시작 로직
+    {
+        isGameOver = false;
+
+        // 게임 시작 시간 기록
+        startTime = Time.time;
+    }
+
+    public void GameOver()   // 게임 종료 로직
+    {
+        isGameOver = true;
+
+        // 게임 종료 시 시간 계산
+        elapsedTime = Time.time - startTime;
+
+        // 최단 시간 갱신
+        if (elapsedTime < bestTime)
+        {
+            bestTime = elapsedTime;
+            Debug.Log("최단 기록! 새로운 기록: " + bestTime + "초");
+        }
+        else
+        {
+            Debug.Log("게임 종료. 소요 시간: " + elapsedTime + "초");
+        }
+    }
+
+    // 플레이어가 점수를 얻었을 때 호출하는 함수
+    public void AddGem(int GemCount)
+    {
+        if (!isGameOver)
+        {
+            stageGem += GemCount;
+            totalGem += GemCount;
+        }
+    }
 }
